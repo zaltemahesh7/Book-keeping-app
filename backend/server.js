6e2caadbb88babddde91e26a1fa05a3e6459baf8
Mahesh Zalte <maheshzalte2000@gmail.com>
2023-08-17 22:37:22 +0530

    API Created and Tested using Postman

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,33 @@
 const express = require('express');
-
 const app = express();
 
+// Routes
+// User Routes
+app.post('/api/users/register', (req, res) => {
+    res.send('Register Routes')
+});
+
+// Login
+app.post('/api/users/login', (req, res) => {
+    res.send('Login Routes')
+});
+
+// Update User
+app.put('/api/users/update', (req, res) => {
+    res.send('update Routes')
+});
+app.delete('/api/users/:id', (req, res) => {
+    res.send('Delete Routes')
+});
+
+// Fetch Users
+app.get('/api/users', (req, res) =>{
+    res.send('Fetch Users')
+})
+
+
+// Server
 const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is listening on Port ${PORT}`);
 })
